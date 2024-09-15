fb4719d1312e4e3efbaad503a78f5ef85384d816
rajudon76 <135225882+rajudon76@users.noreply.github.com>
2024-09-15 15:44:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -25,8 +25,8 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://te
 
 
 global.devs = "258867532400" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "258867532400";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "258867532400";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923071782626";
 
 
 
@@ -56,21 +56,21 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 global.read_status = process.env.AUTO_READ_STATUS || "true"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "258867532400,923xxxxxxxx";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923xxxxxxxx";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://hitdev-qr.onrender.com";
 
 
-global.SESSION_ID = process.env.SESSION_ID || ""
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiaUwwOU80QXdwQXoxVmJjajlhZ21aK3pyWXBKWUdpU0NSMXAyOHRLaiswYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibzE2YStUVUFMbjF4dHJlSmptaDJjemNFb1VrWlVtY3V4OXBtVVAwdXdXWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJXSHRvbzJOeHBPdHVsMk5LNE40NXpGeW84UWJ4Z1NvcW1EYTV4bUZadm5ZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWWDJBNW1mWmZiajd0bm8yN2NiUlBMRlUxM2w5TGcxVGJQMElmTWZGQ1NZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitCT2lrYnRTeUtLMGpoS2ppcW55Nm9BOGhSOFU0UmY5WFF3MlBhNWp3bDQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJnd3hoRFFUOWU3dE85T0RndS8wRVExODhDRzJJQi9GRU1ScEJkdlVTQWs9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUVBsbjhuL3VqTGlyUHRNQ2kzbERKZUd2bGZPL2xwcy82WEViRTVlRWcwcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibTJYanl1MFdGZ3ovMStTTGhMY2NucXpBUytqbE5CcG0xejhzeVE4bzJYbz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBTVkc4R0ZFK25WczhTQ1BlRmZ6SHhRVHJsZ2NJTWJOanlQMU1OaVFIczM5S2NuaUZNZFBNSWJlVGhtQWpQajBZYldLcHRLbjFmNExCRGhiSmdseGh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTgyLCJhZHZTZWNyZXRLZXkiOiI4Umg1OFh2bUFib05ETUhvQ0pUa2RMczdnTXBpSjMrTjB1UXpKU3orY3NvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJRVVVCQms5YVE1R2NVTm1tdVRfbkVnIiwicGhvbmVJZCI6IjIwMDY2NWVmLWY0YTctNDc3My04N2JlLWM5Njk4NmY0YjM2ZiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLZS82VUJFeVk3STJFTVZtVTB1QzE0cm14TmM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNG5kVnBCbzFxRnZ2SVQwN0UxTFNZa1k1T1RnPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6Ik1UUDk3OUhNIiwibWUiOnsiaWQiOiI5MjMwNzE3ODI2MjY6MzZAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoi8J2Vu/CdlpfwnZaU8J2Wi/CdlorwnZaY8J2WmPCdlpTwnZaXIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNQNmY1YU1IRU0vem1yY0dHQjRnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJYVHR0K0ZmcloxZ1FNcVZETGtPeHdJcUZpSXY3eXd2aE04T1RYeitnZDE4PSIsImFjY291bnRTaWduYXR1cmUiOiJjdFMzdHp4UkpWZEFkc2g4RkFIYWpveWZsYkxhcDJmalV1TEdRYVFRRmRFbXp1SXVCQXh5RDFvSHF1NFU3RzhLdTlobmM5bk1lM2hYbDFlVlFkZmpCUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiSXVNUmFoTWlpNXIxV3BJS0lHY3ZPQmladEZJN1hDSkxNOTFFeXNwMjhEck1LQ1JhTVVxR2N5SVc4WXhwN0ZtVmsvb1JqMldrMHhZeXJFNU1iZ0RDalE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMwNzE3ODI2MjY6MzZAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVjA3YmZoWDYyZFlFREtsUXk1RHNjQ0toWWlMKzhzTDRUUERrMTgvb0hkZiJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyNjM5Njg5MywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFJZi8ifQ=="
 module.exports = {
 
   menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: HITDEV-MD Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
-  HANDLERS: process.env.PREFIX  || ".",
+  HANDLERS: process.env.PREFIX  || "",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.0",
-  caption : process.env.CAPTION || "『© 𝑴𝒖𝒛𝒂𝒎𝒎𝒊𝒍 𝑴𝑫 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍』" , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜɪᴛᴅᴇᴠ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • ʜɪᴛᴅᴇᴠ ᴛᴇᴄʜ』*\n youtube.com/@hitdeveloper0"),
+  caption : process.env.CAPTION || "『© Raju Boss"),
  
   author : process.env.PACK_AUTHER|| "Muzammil - MD",
   packname: process.env.PACK_NAME || "",
